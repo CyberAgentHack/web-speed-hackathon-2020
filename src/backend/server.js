7cb03f5c43c2ed7bd274a582457fcb503f037f95
3846masa <3846masahiro+git@gmail.com>
2020-07-22 02:46:38 +0900

    Brotli 圧縮

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import staticGzip from 'express-static-gzip';
 import { apiController } from './controller/api';
 import { spaController } from './controller/spa';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static('dist'));
+app.use(
+  staticGzip('dist', {
+    enableBrotli: true,
+  }),
+);
 app.use(apiController);
 app.use(spaController);
 
