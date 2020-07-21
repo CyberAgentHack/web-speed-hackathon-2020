221a0c239fec718f60ebd60ea984bfded652f963
3846masa <3846masahiro+git@gmail.com>
2020-07-21 19:27:50 +0900

    lodash 依存を軽減

--- src/domains/comment_list/components/CommentList/CommentList.jsx
+++ src/domains/comment_list/components/CommentList/CommentList.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import _ from 'lodash';
 
 import { CommentListItem } from '../CommentListItem';
 
 export function CommentList({ list }) {
   return (
     <ul>
-      {_.map(list, (comment, i) => (
+      {list.map((comment, i) => (
         <li key={i} className="comment-CommentList__item">
           <CommentListItem comment={comment} />
         </li>
