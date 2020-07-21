059a0c0f8860f27105efff5a2aa89476914b2db1
Kazuki Nagai <platong8249@gmail.com>
2020-07-22 00:47:53 +0900

    Introduce production build

--- webpack.config.js
+++ webpack.config.js
@@ -5,50 +5,52 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src', 'app.js'),
+module.exports = (env, { mode }) => {
+  return {
+    entry: path.resolve(__dirname, 'src', 'app.js'),
 
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
-    publicPath: '/',
-  },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js',
+      publicPath: '/',
+    },
 
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
 
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-      'process.env.USE_MOCK_DATA': JSON.stringify(process.env.USE_MOCK_DATA),
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Amida Blog: あみぶろ',
-      template: path.resolve(__dirname, 'src', 'index.html'),
-    }),
-  ],
-
-  module: {
-    rules: [
-      {
-        test: /\.m?jsx?$/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.(png|svg|jpe?g|gif)$/,
-        use: {
-          loader: 'url-loader',
-        },
-      },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        'process.env.USE_MOCK_DATA': JSON.stringify(process.env.USE_MOCK_DATA),
+      }),
+      new HtmlWebpackPlugin({
+        title: 'Amida Blog: あみぶろ',
+        template: path.resolve(__dirname, 'src', 'index.html'),
+      }),
     ],
-  },
 
-  target: 'web',
+    module: {
+      rules: [
+        {
+          test: /\.m?jsx?$/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.(png|svg|jpe?g|gif)$/,
+          use: {
+            loader: 'url-loader',
+          },
+        },
+      ],
+    },
 
-  devtool: 'inline-source-map',
+    target: 'web',
 
-  mode: 'none',
+    devtool: 'inline-source-map',
+
+    mode,
+  };
 };
