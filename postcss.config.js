5d5a2e46bf3fe989e343d7630cc37685491a4d5f
sasaplus1 <sasaplus1@gmail.com>
2020-07-22 00:01:05 +0900

    add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,7 @@ const path = require('path');
 const importPlugin = require('postcss-import');
 const autoprefixer = require('autoprefixer');
 const customProperties = require('postcss-custom-properties');
+const cssnano = require('cssnano');
 
 module.exports = {
   plugins: [
@@ -15,6 +16,10 @@ module.exports = {
     autoprefixer(),
 
     customProperties(),
+
+    cssnano({
+      preset: 'default'
+    }),
   ],
 
   map: process.env.NODE_ENV !== 'production',
