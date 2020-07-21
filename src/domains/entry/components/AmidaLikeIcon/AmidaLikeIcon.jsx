a867b7209b20a67f69c9a413c07c50c43efb899d
Shota Kubota <ta2@o2p.jp>
2020-07-21 21:14:22 +0900

    feat(entry/components): add AmidaLikeIcon components

---
+++ src/domains/entry/components/AmidaLikeIcon/AmidaLikeIcon.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import AmidaLikeIconSvg from '../../../../assets/amida_like_icon.svg';
+
+export function AmidaLikeIcon() {
+  return <img src={AmidaLikeIconSvg} alt="like" width="14" />;
+}
