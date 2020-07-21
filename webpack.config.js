7e25483d8185581b63fa84c2e497ab712aad0011
3846masa <3846masahiro+git@gmail.com>
2020-07-21 19:16:45 +0900

    preact

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
 
   resolve: {
     extensions: ['.js', '.jsx'],
+    alias: {
+      react: 'preact/compat',
+      'react-dom': 'preact/compat',
+    },
   },
 
   plugins: [
