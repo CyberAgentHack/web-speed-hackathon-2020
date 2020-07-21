f23405904ab800ddf0096b759c4f7ba5340670d8
sasaplus1 <sasaplus1@gmail.com>
2020-07-22 02:59:53 +0900

    remove classnames

--- src/foundation/components/ProportionalImage/ProportionalImage.jsx
+++ src/foundation/components/ProportionalImage/ProportionalImage.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import classNames from 'classnames';
+// import classNames from 'classnames';
 
 export function ProportionalImage({
   boxAspectRatio,
   roundedAsCardThumbnail,
   ...imageProps
 }) {
+  const className = roundedAsCardThumbnail
+    ? 'foundation-ProportionalImage foundation-ProportionalImage--rounded-as-card-thumbnail'
+    : 'foundation-ProportionalImage';
+
   return (
     <div
-      className={classNames('foundation-ProportionalImage', {
-        ['foundation-ProportionalImage--rounded-as-card-thumbnail']: roundedAsCardThumbnail,
-      })}
+      className={className}
       style={{ paddingTop: `calc(100% * ${boxAspectRatio})` }}
     >
       <div className="foundation-ProportionalImage__inner">
-        <img className="foundation-ProportionalImage__img" {...imageProps} loading="lazy" />
+        <img
+          className="foundation-ProportionalImage__img"
+          {...imageProps}
+          loading="lazy"
+        />
       </div>
     </div>
   );
