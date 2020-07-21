50d83f7c96b5994af1e26284aa09e0214ad1cc41
ktknest <katou.kennichi@gmail.com>
2020-07-21 19:26:09 +0900

    bye jQuery

--- src/domains/entry/components/FacebookShareButton/FacebookShareButton.jsx
+++ src/domains/entry/components/FacebookShareButton/FacebookShareButton.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import $ from 'jquery';
 
 const FACEBOOK_SDK =
   'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0';
@@ -11,12 +10,13 @@ export function FacebookShareButton() {
       return;
     }
 
-    const script$ = $(
-      `<script crossorigin="anonymous" src=${FACEBOOK_SDK}></script>`,
-    ).appendTo('body');
+    const script = document.createElement('script');
+    script.crossOrigin = 'anonymous';
+    script.src = FACEBOOK_SDK;
+    document.body.appendChild(script);
 
     return () => {
-      script$.remove();
+      document.body.removeChild(script);
     };
   }, []);
 
--- src/domains/entry/components/HatenaBookmarkButton/HatenaBookmarkButton.jsx
+++ src/domains/entry/components/HatenaBookmarkButton/HatenaBookmarkButton.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react';
-import $ from 'jquery';
 
 const HATENA_SDK = 'https://b.st-hatena.com/js/bookmark_button.js';
 
 export function HatenaBookmarkButton({ location }) {
   useEffect(() => {
-    const script$ = $(`<script src=${HATENA_SDK}></script>`).appendTo('body');
+    const script = document.createElement('script');
+    script.src = HATENA_SDK;
+    document.body.appendChild(script);
 
     return () => {
-      script$.remove();
+      document.body.removeChild(script);
     };
   }, []);
 
