f58ea7180ed5ddb45fd7152656814c797c959128
masuP9 <masuP9@gmail.com>
2020-07-21 21:27:25 +0900

    remove jquery

--- src/domains/entry/components/FacebookShareButton/FacebookShareButton.jsx
+++ src/domains/entry/components/FacebookShareButton/FacebookShareButton.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import $ from 'jquery';
 
 const FACEBOOK_SDK =
   'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0';
@@ -11,12 +10,15 @@ export function FacebookShareButton() {
       return;
     }
 
-    const script$ = $(
-      `<script crossorigin="anonymous" src=${FACEBOOK_SDK}></script>`,
-    ).appendTo('body');
+    const script = document.createElement('script');
+    script.setAttribute('crossorigin', 'anonymous');
+    script.setAttribute('src', FACEBOOK_SDK);
+    script.setAttribute('defer', true);
+
+    document.body.appendChild(script);
 
     return () => {
-      script$.remove();
+      document.body.removeChild(script);
     };
   }, []);
 
--- src/domains/entry/components/TwitterShareButton/TwitterShareButton.jsx
+++ src/domains/entry/components/TwitterShareButton/TwitterShareButton.jsx
@@ -1,16 +1,22 @@
 import React, { useEffect } from 'react';
-import $ from 'jquery';
 
 const TWITTER_SDK = 'https://platform.twitter.com/widgets.js';
 
 export function TwitterShareButton() {
   useEffect(() => {
-    const script$ = $(`<script src=${TWITTER_SDK}></script>`).appendTo('body');
+    const script = document.createElement('script');
+    script.setAttribute('src', TWITTER_SDK);
+    script.setAttribute('defer', true);
+
+    document.body.appendChild(script);
 
     return () => {
-      script$.remove();
+      document.body.removeChild(script);
     };
   }, []);
+
+  const href = `https://twitter.com/intent/tweet?
+  original_referer=http%3A%2F%2Flocalhost%3A3000%2Fb0007%2Fentry%2Fe0039&ref_src=twsrc%5Etfw&text=%E6%97%A5%E8%A8%98%20%2339%20-%20Mock%20User%207%20-%20Amida%20Blog%3A%20%E3%81%82%E3%81%BF%E3%81%B6%E3%82%8D&tw_p=tweetbutton&url=http%3A%2F%2Flocalhost%3A3000%2Fb0007%2Fentry%2Fe0039`;
 
   return (
     <div className="entry-TwitterShareButton">
