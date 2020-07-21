4dab38c2b9e76c1a03df36b8cff132a84703ed8b
uenitty <uenitty@gmail.com>
2020-07-21 21:16:44 +0900

    loadable

--- src/foundation/routes.jsx
+++ src/foundation/routes.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import loadable from '@loadable/component'
 
-import { Entrance } from '../pages/entrance';
-import { BlogHome } from '../pages/blog_home';
-import { Entry } from '../pages/entry';
-import { NotFound } from '../pages/not_found';
+const Entrance = loadable(() => import( '../pages/entrance'), {
+  resolveComponent: (components) => components.Entrance,
+});
+const BlogHome = loadable(() => import( '../pages/blog_home'), {
+  resolveComponent: (components) => components.BlogHome,
+});
+const Entry = loadable(() => import( '../pages/entry'), {
+  resolveComponent: (components) => components.Entry,
+});
+const NotFound = loadable(() => import( '../pages/not_found'), {
+  resolveComponent: (components) => components.NotFound,
+});
 
 export function Routes() {
   const error = useSelector((state) => state.error.toJS());
