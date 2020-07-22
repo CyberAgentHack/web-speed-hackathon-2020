1c232000f31f652090200c77b333fd162156712f
3846masa <3846masahiro+git@gmail.com>
2020-07-22 09:03:09 +0900

    dnscache

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import staticGzip from 'express-static-gzip';
+import dnscache from 'dnscache';
 import { apiController } from './controller/api';
 import { spaController } from './controller/spa';
+
+dnscache({
+  enable: true,
+  ttl: 24 * 60 * 60,
+  cachesize: 1000,
+});
 
 const PORT = process.env.PORT || 3000;
 const app = express();
