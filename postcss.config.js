e6c0f83ed2d4778818bca3e8f308ee9f88f8538d
hiteto218 <hidemaruhidekyun@gmail.com>
2020-07-22 17:36:00 +0900

    Add: minify css and unused css

--- postcss.config.js
+++ postcss.config.js
@@ -6,16 +6,28 @@ const importPlugin = require('postcss-import');
 const autoprefixer = require('autoprefixer');
 const customProperties = require('postcss-custom-properties');
 
+const srcPath = path.resolve(__dirname, 'src');
+
 module.exports = {
   plugins: [
     importPlugin({
-      root: path.resolve(__dirname, 'src'),
+      root: srcPath,
     }),
 
     autoprefixer(),
 
     customProperties(),
+    require('cssnano')({
+      preset: 'default',
+    }),
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        `${srcPath}/**/*.jsx`,
+        `${srcPath}/**/*.js`,
+        `${srcPath}/index.html`,
+      ],
+      css: [`${srcPath}/app.css`],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
   ],
-
-  map: true,
 };
