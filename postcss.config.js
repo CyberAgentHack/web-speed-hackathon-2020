33e774e1fe2f1ee7ab6c81d9eef07c51a25e70a7
3846masa <3846masahiro+git@gmail.com>
2020-07-22 18:12:47 +0900

    cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -5,17 +5,16 @@ const path = require('path');
 const importPlugin = require('postcss-import');
 const autoprefixer = require('autoprefixer');
 const customProperties = require('postcss-custom-properties');
+const cssnano = require('cssnano');
 
 module.exports = {
   plugins: [
     importPlugin({
       root: path.resolve(__dirname, 'src', 'client'),
     }),
-
     autoprefixer(),
-
     customProperties(),
+    cssnano({ preset: 'default' }),
   ],
-
-  map: true,
+  map: false,
 };
