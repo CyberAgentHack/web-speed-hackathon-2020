36b2a31d30d5180867ecf2b2d1435ed78226fd16
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-07-22 19:24:29 +0900

    Reimplement helpers/shuffle correctly

--- src/foundation/helpers/shuffle.js
+++ src/foundation/helpers/shuffle.js
@@ -1,4 +1,10 @@
-// FIXME Implement lodash/shuffle
 export function shuffle(items) {
+  for (let i = items.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const tmp = items[i];
+    items[i] = items[j];
+    items[j] = tmp;
+  }
+
   return items;
 }
