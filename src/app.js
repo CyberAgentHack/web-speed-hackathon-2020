924d05a62d591b845942cdff5657887b79a962fa
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-07-22 18:30:03 +0900

    Drop mock import

--- src/app.js
+++ src/app.js
@@ -1,13 +1,8 @@
 import './foundation/polyfills';
 
 import { render } from './foundation/render';
-import { setupMockAPIData } from './foundation/setupMockAPIDatagss';
 
 function init() {
-  if (process.env.USE_MOCK_DATA === 'true') {
-    setupMockAPIData();
-  }
-
   render();
 }
 
