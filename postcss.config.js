2cba5c095a369ce60dd039a18aabd6b6d57f0245
ktknest <katou.kennichi@gmail.com>
2020-07-22 18:26:47 +0900

    cssnano and disabled map

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,7 @@ const path = require('path');
 const importPlugin = require('postcss-import');
 const autoprefixer = require('autoprefixer');
 const customProperties = require('postcss-custom-properties');
+const cssnano = require('cssnano');
 
 module.exports = {
   plugins: [
@@ -15,7 +16,7 @@ module.exports = {
     autoprefixer(),
 
     customProperties(),
-  ],
 
-  map: true,
+    cssnano(),
+  ],
 };
