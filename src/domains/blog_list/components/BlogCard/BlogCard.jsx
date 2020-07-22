1db821e72683a42e7f96b91d46187afe08ddf850
Kazuki Nagai <platong8249@gmail.com>
2020-07-22 17:11:03 +0900

    BlogCardのメモ化

--- src/domains/blog_list/components/BlogCard/BlogCard.jsx
+++ src/domains/blog_list/components/BlogCard/BlogCard.jsx
@@ -3,18 +3,21 @@ import { Link } from 'react-router-dom';
 
 import { ProportionalImage } from '../../../../foundation/components/ProportionalImage';
 
-export function BlogCard({ blog }) {
-  return (
-    <Link className="blog-list-BlogCard" to={`/${blog.blog_id}`}>
-      <div className="blog-list-BlogCard__thumbnail">
-        <ProportionalImage
-          src={blog.image || ''}
-          alt=""
-          boxAspectRatio={9 / 16}
-          roundedAsCardThumbnail
-        />
-      </div>
-      <p className="blog-list-BlogCard__title">{blog.nickname || ''}</p>
-    </Link>
-  );
-}
+export const BlogCard = React.memo(
+  ({ blog }) => {
+    return (
+      <Link className="blog-list-BlogCard" to={`/${blog.blog_id}`}>
+        <div className="blog-list-BlogCard__thumbnail">
+          <ProportionalImage
+            src={blog.image || ''}
+            alt=""
+            boxAspectRatio={9 / 16}
+            roundedAsCardThumbnail
+          />
+        </div>
+        <p className="blog-list-BlogCard__title">{blog.nickname || ''}</p>
+      </Link>
+    );
+  },
+  (prev, next) => prev.blog.blog_id === next.blog.blog_id,
+);
