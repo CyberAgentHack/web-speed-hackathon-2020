6dee07d84ae635206935d7d99c6f3619576d3d85
Kazuki Nagai <platong8249@gmail.com>
2020-07-22 17:17:44 +0900

    AmidaImageのメモ化

--- src/foundation/components/AmidaImage/AmidaImage.jsx
+++ src/foundation/components/AmidaImage/AmidaImage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import AmidaPng from '../../../assets/amida.png';
 
-export default function AmidaImage() {
+const AmidaImage = React.memo(() => {
   return <img src={AmidaPng} className="Entrance__hero-logo" alt="" />;
-}
+});
+
+export default AmidaImage;
