4d881909fbb59de55ce1e67732e1a40c6d8e6f76
naro143 <hoshipuri0902@gmail.com>
2020-09-22 19:08:07 +0900

    fix: test function

--- api/server.js
+++ api/server.js
@@ -1,44 +1,11 @@
 'use strict';
 
 const express = require('express');
-const line = require('@line/bot-sdk');
 const PORT = process.env.PORT || 3000;
-
-const config = {
-    channelSecret: '作成したBOTのチャンネルシークレット',
-    channelAccessToken: '作成したBOTのチャンネルアクセストークン'
-};
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello LINE BOT!(GET)')); //ブラウザ確認用(無くても問題ない)
-app.post('/webhook', line.middleware(config), (req, res) => {
-    console.log(req.body.events);
-
-    //ここのif分はdeveloper consoleの"接続確認"用なので削除して問題ないです。
-    if(req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff'){
-        res.send('Hello LINE BOT!(POST)');
-        console.log('疎通確認用');
-        return; 
-    }
-
-    Promise
-      .all(req.body.events.map(handleEvent))
-      .then((result) => res.json(result));
-});
-
-const client = new line.Client(config);
-
-async function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null);
-  }
-
-  return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: event.message.text //実際に返信の言葉を入れる箇所
-  });
-}
+app.get('/webhook', (req, res) => res.send('Hello LINE BOT!(GET)'));
 
 (process.env.NOW_REGION) ? module.exports = app : app.listen(PORT);
 console.log(`Server running at ${PORT}`);
